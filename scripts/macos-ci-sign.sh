a7ed8916d1b582d1536f95f739bc3ce09427dbc7
Rithvik Vibhu <rithvikvibhu@gmail.com>
2023-02-14 13:17:33 +0530

    ci: cirrus 8/n

--- scripts/macos-ci-sign.sh
+++ scripts/macos-ci-sign.sh
@@ -18,6 +18,12 @@ fi
 
 echo "using rcodesign: $program"
 
+# Sign
+echo "[*] Signing..."
 $program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path resources/entitlements.plist release/Bob.app
 
-echo "Done."
+# Notarize
+echo "[*] Notarizing..."
+$program notary-submit --api-key-path /tmp/appstoreconnect_key.json --staple release/Bob.app
+
+echo "[*] Done."
