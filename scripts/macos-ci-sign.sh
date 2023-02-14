bef9b4a3a2b1b372d069e5d1bfe80bc7cb0618b8
Rithvik Vibhu <rithvikvibhu@gmail.com>
2023-02-14 14:31:29 +0530

    ci: cirrus 17/n

--- scripts/macos-ci-sign.sh
+++ scripts/macos-ci-sign.sh
@@ -19,18 +19,35 @@ fi
 entitlements="`pwd`/resources/entitlements.plist"
 release_dir="`pwd`/release/Bob.app"
 
-echo "pwd: `pwd`"
 echo "using rcodesign: $program"
-ls -la
 echo "using entitlements: $entitlements"
-ls -la resources/
 echo "using release_dir: $release_dir"
-ls -la release/
-
-ls -la /tmp/
 
 # Sign
 echo "[*] Signing..."
+
+additional_files=(
+    "Contents/Resources/app.asar.unpacked/node_modules/leveldown/build/Release/leveldown.node"
+    "Contents/Resources/app.asar.unpacked/node_modules/leveldown/build/node_gyp_bins/python3"
+    "Contents/Resources/app.asar.unpacked/node_modules/leveldown/prebuilds/darwin-x64/node.napi.node"
+    "Contents/Resources/app.asar.unpacked/node_modules/mrmr/build/Release/mrmr.node"
+    "Contents/Resources/app.asar.unpacked/node_modules/mrmr/build/node_gyp_bins/python3"
+    "Contents/Resources/app.asar.unpacked/node_modules/node-hid/build/Release/HID.node"
+    "Contents/Resources/app.asar.unpacked/node_modules/node-hid/build/node_gyp_bins/python3"
+    "Contents/Resources/app.asar.unpacked/node_modules/bdb/build/Release/leveldown.node"
+    "Contents/Resources/app.asar.unpacked/node_modules/bdb/build/node_gyp_bins/python3"
+    "Contents/Resources/app.asar.unpacked/node_modules/bcrypto/build/Release/bcrypto.node"
+    "Contents/Resources/app.asar.unpacked/node_modules/bcrypto/build/node_gyp_bins/python3"
+    "Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt"
+)
+
+# Sign each file individally, rcodesign does not pick them up (bug)
+echo "[*] Signing ${#additional_files[@]} additional files first..."
+for s in "${additional_files[@]}"; do
+    $program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path $entitlements $release_dir/$s
+done
+
+echo "[*] Signing final bundle..."
 $program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path $entitlements $release_dir
 
 # Notarize
