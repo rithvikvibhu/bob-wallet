bfc5fc8c437e354f6edc177d4aa92ce53fddcd82
Rithvik Vibhu <rithvikvibhu@gmail.com>
2023-02-14 13:02:53 +0530

    ci: cirrus 7/n

--- scripts/macos-ci-sign.sh
+++ scripts/macos-ci-sign.sh
@@ -18,6 +18,6 @@ fi
 
 echo "using rcodesign: $program"
 
-rcodesign sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path resources/entitlements.plist release/Bob.app
+$program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path resources/entitlements.plist release/Bob.app
 
 echo "Done."
