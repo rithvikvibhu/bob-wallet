4b1c990b620d1e714c3ace5cbd4f4434a9739dfd
Rithvik Vibhu <rithvikvibhu@gmail.com>
2024-02-03 08:59:51 +0530

    ci: cirrus test 1/n2

--- scripts/macos-ci-sign.sh
+++ scripts/macos-ci-sign.sh
@@ -26,30 +26,30 @@ echo "using release_dir: $release_dir"
 # Sign
 echo "[*] Signing..."
 
-additional_files=(
-    "Contents/Resources/app.asar.unpacked/node_modules/leveldown/build/Release/leveldown.node"
-    "Contents/Resources/app.asar.unpacked/node_modules/leveldown/build/node_gyp_bins/python3"
-    "Contents/Resources/app.asar.unpacked/node_modules/leveldown/prebuilds/darwin-x64/node.napi.node"
-    "Contents/Resources/app.asar.unpacked/node_modules/mrmr/build/Release/mrmr.node"
-    "Contents/Resources/app.asar.unpacked/node_modules/mrmr/build/node_gyp_bins/python3"
-    "Contents/Resources/app.asar.unpacked/node_modules/node-hid/build/Release/HID.node"
-    "Contents/Resources/app.asar.unpacked/node_modules/node-hid/build/node_gyp_bins/python3"
-    "Contents/Resources/app.asar.unpacked/node_modules/bdb/build/Release/leveldown.node"
-    "Contents/Resources/app.asar.unpacked/node_modules/bdb/build/node_gyp_bins/python3"
-    "Contents/Resources/app.asar.unpacked/node_modules/bcrypto/build/Release/bcrypto.node"
-    "Contents/Resources/app.asar.unpacked/node_modules/bcrypto/build/node_gyp_bins/python3"
-    "Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt"
-    "Contents/Frameworks/Electron\ Framework.framework/Versions/A/Libraries/libffmpeg.dylib"
-)
+# additional_files=(
+#     "Contents/Resources/app.asar.unpacked/node_modules/leveldown/build/Release/leveldown.node"
+#     "Contents/Resources/app.asar.unpacked/node_modules/leveldown/build/node_gyp_bins/python3"
+#     "Contents/Resources/app.asar.unpacked/node_modules/leveldown/prebuilds/darwin-x64/node.napi.node"
+#     "Contents/Resources/app.asar.unpacked/node_modules/mrmr/build/Release/mrmr.node"
+#     "Contents/Resources/app.asar.unpacked/node_modules/mrmr/build/node_gyp_bins/python3"
+#     "Contents/Resources/app.asar.unpacked/node_modules/node-hid/build/Release/HID.node"
+#     "Contents/Resources/app.asar.unpacked/node_modules/node-hid/build/node_gyp_bins/python3"
+#     "Contents/Resources/app.asar.unpacked/node_modules/bdb/build/Release/leveldown.node"
+#     "Contents/Resources/app.asar.unpacked/node_modules/bdb/build/node_gyp_bins/python3"
+#     "Contents/Resources/app.asar.unpacked/node_modules/bcrypto/build/Release/bcrypto.node"
+#     "Contents/Resources/app.asar.unpacked/node_modules/bcrypto/build/node_gyp_bins/python3"
+#     "Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt"
+#     "Contents/Frameworks/Electron\ Framework.framework/Versions/A/Libraries/libffmpeg.dylib"
+# )
 
-# Sign each file individally, rcodesign does not pick them up (bug)
-echo "[*] Signing ${#additional_files[@]} additional files first..."
-for s in "${additional_files[@]}"; do
-    $program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path $entitlements $release_dir/$s
-done
+# # Sign each file individally, rcodesign does not pick them up (bug)
+# echo "[*] Signing ${#additional_files[@]} additional files first..."
+# for s in "${additional_files[@]}"; do
+#     $program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path $entitlements $release_dir/$s
+# done
 
 echo "[*] Signing final bundle..."
-$program sign --remote-signer --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path $entitlements $release_dir
+$program sign --remote-public-key-pem-file /tmp/signing_public_key.pem --code-signature-flags runtime --entitlements-xml-path $entitlements $release_dir
 
 # Notarize
 echo "[*] Notarizing..."
